d9f4ab7f815467cf4771e7c831d9e544099e7717
Maëlle Rabouan <maelle.rabouan@gmail.com>
2026-01-20 18:13:27 +0100

    test err cors

--- netlify/functions/sheet-proxy.js
+++ netlify/functions/sheet-proxy.js
@@ -1,29 +1,27 @@
 import fetch from "node-fetch";
 
-export async function handler(event, context) {
+export async function handler(event) {
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
-
-  const body = JSON.parse(event.body);
 
   const response = await fetch(
     "https://script.google.com/macros/s/AKfycbzhQaEurnr09b20ASqoWaGUUr18LNuFK6HziWuYNEOQBiVOMl8ldFE4iHdN8oi-kLk4tw/exec",
     {
       method: "POST",
-      body: JSON.stringify(body),
       headers: { "Content-Type": "application/json" },
+      body: event.body,
     },
   );
 
-  const data = await response.json();
+  const data = await response.text();
 
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*", // Important pour Netlify
+      "Access-Control-Allow-Origin": "*",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data),
+    body: data,
   };
 }
